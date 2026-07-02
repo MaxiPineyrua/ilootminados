ebe53fbff8d27f0b9e2c0b36afbd5f08a3ad7d0f
MaxiPineyrua <111310426+MaxiPineyrua@users.noreply.github.com>
2026-07-01 23:36:26 -0300

    Primer Commit - React básico casi sin ediciones.

---
+++ ilootminados/src/App.jsx
@@ -0,0 +1,11 @@
+//Función principal de mi app.
+function App() {
+  return (
+    <div>
+      <h1>Bienvenido a iLOOTminados !</h1>
+      <p>Próximamente aquí encontrarás los mejores veladores gamer.</p>
+    </div>
+  );
+}
+//Exporto la función para poder usarla en otros archivos.
+export default App;
