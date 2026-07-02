bc856078b10c3007b9768d54725ec35acfb77f37
MaxiPineyrua <111310426+MaxiPineyrua@users.noreply.github.com>
2026-07-02 16:50:54 -0300

    Creación de la página Home y de los archivos .json

---
+++ ilootminados/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+//Página principal de mi app.
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenidos a iLOOTminados</h1>
+      <p>Encontrá los mejores veladores gamer</p>
+    </div>
+  );
+}
+
+//Exporto la página para usarla luego.
+export default Home;
